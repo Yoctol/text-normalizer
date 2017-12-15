0e08637211a6cc400543a66586680a3624b5f0ae
Solumilken <s916526000@gmail.com>
2017-12-15 12:55:41 +0800

    rename filter as text normalizers and register

---
+++ text_normalizer_library/__init__.py
@@ -0,0 +1,13 @@
+from .basic_text_normalizers import (
+    whitespace_char_text_normalizer,
+    chinese_punctuation_text_normalizer,
+    english_punctuation_text_normalizer,
+    int_text_normalizer,
+    float_text_normalizer,
+)
+from .date_text_normalizers import (
+    date_text_normalizer_yymmdd,
+)
+from .time_text_normalizers import (
+    time_text_normalizer_hhmm,
+)
