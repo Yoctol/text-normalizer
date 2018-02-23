d2e7179dbbf6d28853fe5999c9aeb067fd7d40a4
Solumilken <s916526000@gmail.com>
2018-02-23 14:35:24 +0800

    more precisely: added full and simplified version

--- text_normalizer_collection_library/__init__.py
+++ text_normalizer_collection_library/__init__.py
@@ -1,3 +1,6 @@
 from .basic_text_normalizer_collection import basic_text_normalizer_collection
 from .eng_basic_text_normalizer_collection import eng_basic_text_normalizer_collection
-from .punctuation_keeping_text_normalizer_collection import punctuation_keeping_text_normalizer_collection
+from .punctuation_keeping_text_normalizer_collection import (
+    full_punctuation_keeping_text_normalizer_collection,
+    simplified_punctuation_keeping_text_normalizer_collection,
+)
--- text_normalizer_collection_library/punctuation_keeping_text_normalizer_collection.py
+++ text_normalizer_collection_library/punctuation_keeping_text_normalizer_collection.py
@@ -1,5 +1,6 @@
 from ..base_text_normalizer_collection import BaseTextNormalizerCollection
 from ..text_normalizer_library.punctuation_mapping_text_normalizers import (
+    full_punctuation_mapping_text_normalizer,
     simplified_punctuation_mapping_text_normalizer,
 )
 from ..text_normalizer_library.basic_text_normalizers import (
@@ -14,9 +15,20 @@ from ..text_normalizer_library.eng_lowercase_text_normalizer import (
     eng_lowercase_text_normalizer,
 )
 
+full_punctuation_keeping_text_normalizer_collection = BaseTextNormalizerCollection()
+full_punctuation_keeping_text_normalizer_collection.add_text_normalizers(
+    text_normalizers=[
+        eng_lowercase_text_normalizer,
+        full_punctuation_mapping_text_normalizer,
+        float_text_normalizer,
+        int_text_normalizer,
+        whitespace_char_text_normalizer,
+        pure_strip_text_normalizer,
+    ],
+)
 
-punctuation_keeping_text_normalizer_collection = BaseTextNormalizerCollection()
-punctuation_keeping_text_normalizer_collection.add_text_normalizers(
+simplified_punctuation_keeping_text_normalizer_collection = BaseTextNormalizerCollection()
+simplified_punctuation_keeping_text_normalizer_collection.add_text_normalizers(
     text_normalizers=[
         eng_lowercase_text_normalizer,
         simplified_punctuation_mapping_text_normalizer,
