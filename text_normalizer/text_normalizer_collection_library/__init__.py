4f9ecf1b66ee3b6215643c9f8ff9a54415820068
Solumilken <s916526000@gmail.com>
2018-04-16 11:42:58 +0800

    fit flake8

--- text_normalizer/text_normalizer_collection_library/__init__.py
+++ text_normalizer/text_normalizer_collection_library/__init__.py
@@ -1,14 +1,14 @@
-from .basic_text_normalizer_collection import (
+from .basic_text_normalizer_collection import (  # noqa
     basic_text_normalizer_collection,
     number_with_digits_text_normalizer_collection,
 )
-from .eng_basic_text_normalizer_collection import eng_basic_text_normalizer_collection
-from .punctuation_keeping_text_normalizer_collection import (
+from .eng_basic_text_normalizer_collection import eng_basic_text_normalizer_collection  # noqa
+from .punctuation_keeping_text_normalizer_collection import (  # noqa
     full_punctuation_keeping_text_normalizer_collection,
     simplified_punctuation_keeping_text_normalizer_collection,
     number_with_digits_n_simplified_punctuation_text_normalizer_collection,
 )
-from .charactor_text_normalizer_collection import (
+from .charactor_text_normalizer_collection import (  # noqa
     chinese_charactor_text_normalizer_collection_1,
     chinese_charactor_text_normalizer_collection_2,
     chinese_charactor_text_normalizer_collection_3,
--- text_normalizer/text_normalizer_library/__init__.py
+++ text_normalizer/text_normalizer_library/__init__.py
@@ -1,26 +1,26 @@
-from .basic_text_normalizers import (
+from .basic_text_normalizers import (  # noqa
     whitespace_char_text_normalizer,
 )
-from .punctuation_text_normalizers import (
+from .punctuation_text_normalizers import (  # noqa
     chinese_punctuation_text_normalizer,
     english_punctuation_text_normalizer,
     all_punctuation_text_normalizer,
     all_punctuation_without_endpoint_text_normalizer,
     all_punctuation_without_underscore_text_normalizer
 )
-from .date_text_normalizers import (
+from .date_text_normalizers import (  # noqa
     date_text_normalizer_yymmdd,
 )
-from .time_text_normalizers import (
+from .time_text_normalizers import (  # noqa
     time_text_normalizer_hhmm,
 )
-from .identity_text_normalizer import identity_text_normalizer
-from .eng_lowercase_text_normalizer import eng_lowercase_text_normalizer
-from .punctuation_mapping_text_normalizers import (
+from .identity_text_normalizer import identity_text_normalizer  # noqa
+from .eng_lowercase_text_normalizer import eng_lowercase_text_normalizer  # noqa
+from .punctuation_mapping_text_normalizers import (  # noqa
     full_punctuation_mapping_text_normalizer,
     simplified_punctuation_mapping_text_normalizer,
 )
-from .number_text_normalizers import (
+from .number_text_normalizers import (  # noqa
     int_text_normalizer,
     float_text_normalizer,
     int_with_digit_text_normalizer,
@@ -30,6 +30,6 @@ from .number_text_normalizers import (
     int_with_digit_n_space_text_normalizer,
     float_with_digit_n_space_text_normalizer,
 )
-from .strip_text_normalizers import (
+from .strip_text_normalizers import (  # noqa
     pure_strip_text_normalizer,
 )
