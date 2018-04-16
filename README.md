efd2fa032b15047c37764139d17ca8ef261bd6d0
Solumilken <s916526000@gmail.com>
2018-04-16 11:43:15 +0800

    setup this repo

---
+++ README.md
@@ -0,0 +1,17 @@
+# UTTUT
+
+[![travis][travis-image]][travis-url]
+[![pypi][pypi-image]][pypi-url]
+
+[travis-image]: https://img.shields.io/travis/Yoctol/text-normalizer.svg?style=flat
+[travis-url]: https://travis-ci.org/Yoctol/text-normalizer
+[pypi-image]: https://img.shields.io/pypi/v/uttut.svg?style=flat
+[pypi-url]: https://pypi.python.org/pypi/text-normalizer
+
+Normalizer your text String.
+
+## Install
+Use Python3
+```
+pip install text-normalizer
+```
---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    long_description = open("README.md").read()
+except IOError:
+    long_description = ""
+
+about = {}
+with open(os.path.join(here, "text_normalizer", "__version__.py")) as f:
+    exec(f.read(), about)
+
+setup(
+    name="text-normalizer",
+    version=about["__version__"],
+    description="Yoctol Natural Language Text Normalizer",
+    license="MIT",
+    author="Solumilken",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
