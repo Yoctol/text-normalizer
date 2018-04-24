ddafca1cb98e22ec9bf329363c9d4286a971591f
Solumilken <s916526000@gmail.com>
2018-04-24 15:10:47 +0800

    normalize fullwidth english charactor

--- text_normalizer/text_normalizer_factory/eng_lowercase_text_normalizer.py
+++ text_normalizer/text_normalizer_factory/eng_lowercase_text_normalizer.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict, Tuple
 import re
 
 from .base_text_normalizer import BaseTextNormalizer
@@ -8,21 +8,55 @@ class EngLowercaseTextNormalizer(BaseTextNormalizer):
 
     def __init__(self, name='eng_lowercase'):
         super().__init__(name=name, denormalizable=True)
-        self.pattern = '[a-zA-Z]+'
+        self.fullwidth_uppercase = "ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ"
+        self.fullwidth_lowercase = "ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ"
+        self.halfwidth_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        self.halfwidth_lowercase = "abcdefghijklmnopqrstuvwxyz"
+
+        self.pattern = "[a-zA-Z{}{}]+".format(
+            self.fullwidth_uppercase,
+            self.fullwidth_lowercase,
+        )
         self.findall_prog = re.compile(self.pattern)
+        self.mapping_table = self.gen_table()
+
+    def gen_table(self) -> Dict[str, str]:
+        table = {}
+        for index in range(26):
+            table[self.fullwidth_uppercase[index]] = \
+                self.halfwidth_lowercase[index]
+            table[self.fullwidth_lowercase[index]] = \
+                self.halfwidth_lowercase[index]
+            table[self.halfwidth_uppercase[index]] = \
+                self.halfwidth_lowercase[index]
+        return table
+
+    def lowercase(self, sentence: str) -> str:
+        output = []
+        for char in sentence:
+            if char in self.mapping_table:
+                output.append(self.mapping_table[char])
+            else:
+                output.append(char)
+        return ''.join(output)
 
     def normalize(
             self,
             sentence: str,
-        ) -> (str, List[dict]):
+        ) -> Tuple[str, List[dict]]:
         eng_words = self.findall_prog.findall(sentence)
         if len(eng_words) == 0:
-            return sentence.lower(), None
+            return sentence, None
         else:
             meta = []
             for eng_word in eng_words:
-                meta.append({'before': eng_word, 'after': eng_word.lower()})
-            return sentence.lower(), meta
+                meta.append(
+                    {
+                        'before': eng_word,
+                        'after': self.lowercase(eng_word),
+                    },
+                )
+            return self.lowercase(sentence), meta
 
     def denormalize(
             self,
--- text_normalizer/text_normalizer_factory/test/test_eng_lowercase_text_normalizer.py
+++ text_normalizer/text_normalizer_factory/test/test_eng_lowercase_text_normalizer.py
@@ -8,79 +8,107 @@ class EngLowercaseTextNormalizerTestCase(TestCase):
     def setUp(self):
         self.eng_lowercase_text_normalizer = EngLowercaseTextNormalizer()
 
+    def test_lowercase(self):
+        test_cases = [
+            (
+                "ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ",
+                "abcdefghijklmnopqrstuvwxyz",
+            ),
+            (
+                "ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ",
+                "abcdefghijklmnopqrstuvwxyz",
+            ),
+            (
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                "abcdefghijklmnopqrstuvwxyz",
+            ),
+        ]
+        for test_case in test_cases:
+            with self.subTest(test_case=test_case):
+                self.assertEqual(
+                    test_case[1],
+                    self.eng_lowercase_text_normalizer.lowercase(
+                        sentence=test_case[0],
+                    ),
+                )
+
     def test_normalize_n_denormalize_0(self):
-        result = self.eng_lowercase_text_normalizer.normalize(
-            '哈囉 AAB 123 Cddef 哈囉 >< ???',
-        )
-        self.assertEqual(
-            ('哈囉 aab 123 cddef 哈囉 >< ???',
-             [
-                 {
-                     'before': 'AAB',
-                     'after': 'aab',
-                 },
-                 {
-                     'before': 'Cddef',
-                     'after': 'cddef',
-                 },
-             ],
-             ),
-            result,
-        )
-        result = self.eng_lowercase_text_normalizer.denormalize(
-            sentence=result[0],
-            meta=result[1],
-        )
-        self.assertEqual(
-            '哈囉 AAB 123 Cddef 哈囉 >< ???',
-            result,
-        )
+        test_cases = [
+            (
+                "哈囉 AAB 123 Cddef 哈囉 >< ???",
+                (
+                    "哈囉 aab 123 cddef 哈囉 >< ???",
+                    [
+                        {
+                            "before": "AAB",
+                            "after": "aab",
+                        },
+                        {
+                            "before": "Cddef",
+                            "after": "cddef",
+                        },
+                    ],
+                ),
+                "哈囉 AAB 123 Cddef 哈囉 >< ???",
+            ),
+            (
+                "AAB 123 哈囉 Cddef 456 ffecI",
+                (
+                    "aab 123 哈囉 cddef 456 ffeci",
+                    [
+                        {
+                            "before": "AAB",
+                            "after": "aab",
+                        },
+                        {
+                            "before": "Cddef",
+                            "after": "cddef",
+                        },
+                        {
+                            "before": "ffecI",
+                            "after": "ffeci",
+                        },
+                    ],
+                ),
+                "AAB 123 哈囉 Cddef 456 ffecI",
+            ),
+            (
+                "家豪大大亂入吃雞排",
+                ("家豪大大亂入吃雞排", None),
+                "家豪大大亂入吃雞排",
+            ),
+            (
+                "ａbc",
+                (
+                    "abc",
+                    [
+                        {
+                            "before": "ａbc",
+                            "after": "abc",
+                        },
+                    ],
+                ),
+                "ａbc",
+            ),
+        ]
 
-    def test_normalize_n_denormalize_1(self):
-        result = self.eng_lowercase_text_normalizer.normalize(
-            'AAB 123 哈囉 Cddef 456 ffecI',
-        )
-        self.assertEqual(
-            ('aab 123 哈囉 cddef 456 ffeci',
-             [
-                 {
-                     'before': 'AAB',
-                     'after': 'aab',
-                 },
-                 {
-                     'before': 'Cddef',
-                     'after': 'cddef',
-                 },
-                 {
-                     'before': 'ffecI',
-                     'after': 'ffeci',
-                 },
-             ],
-             ),
-            result,
-        )
-        result = self.eng_lowercase_text_normalizer.denormalize(
-            sentence=result[0],
-            meta=result[1],
-        )
-        self.assertEqual(
-            'AAB 123 哈囉 Cddef 456 ffecI',
-            result,
-        )
-
-    def test_normalize_n_denormalize_2(self):
-        result = self.eng_lowercase_text_normalizer.normalize(
-            '家豪大大亂入吃雞排',
-        )
-        self.assertEqual(
-            ('家豪大大亂入吃雞排', None),
-            result,
-        )
-        result = self.eng_lowercase_text_normalizer.denormalize(
-            sentence=result[0],
-            meta=result[1],
-        )
-        self.assertEqual(
-            '家豪大大亂入吃雞排',
-            result,
-        )
+        for test_case in test_cases:
+            with self.subTest(
+                test_case="normalize {}".format(test_case[0]),
+            ):
+                self.assertEqual(
+                    test_case[1],
+                    self.eng_lowercase_text_normalizer.normalize(
+                        sentence=test_case[0],
+                    ),
+                )
+            with self.subTest(
+                test_case="denormalize {}".format(test_case[0]),
+            ):
+                self.assertEqual(
+                    test_case[2],
+                    self.eng_lowercase_text_normalizer.denormalize(
+                        sentence=test_case[1][0],
+                        meta=test_case[1][1],
+                    ),
+                )
